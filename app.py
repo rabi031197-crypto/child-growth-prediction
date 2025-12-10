a065306a02e04cc863866b3b16731cf8e37ea980
rabi031197-crypto <rabi031197@gmail.com>
2025-12-10 13:39:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import joblib
 
 models = {
-  'h1': joblib.load("models/model_height.joblib"),
-  'h2': joblib.load("models/model_height_2y.joblib"),
-  'w1': joblib.load("models/model_weight.joblib"),
-  'w2': joblib.load("models/model_weight_2y.joblib")
+  'h1': joblib.load("model_height.joblib"),
+  'h2': joblib.load("model_height_2y.joblib"),
+  'w1': joblib.load("model_weight.joblib"),
+  'w2': joblib.load("model_weight_2y.joblib")
 }
-scaler = joblib.load("models/scaler.joblib")
+scaler = joblib.load("scaler.joblib")
